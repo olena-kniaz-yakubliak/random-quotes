ce631433f23b0e3ffc4ae721e05f7c09671d006d
kniazivna <olenkaknyaz@gmail.com>
2026-07-10 20:23:04 +0300

    change arr of quotes, refactor js, add styles

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 "use strict";
 
 const quotes = [
-  "Lorem5 ipsum dolor sit amet, consectetur adipiscing elit.1111111111111111111",
-  "Lorem10 ipsum dolor sit amet, consectetur adipiscing elit.222222222222",
-  "lorem20 ipsum dolor sit amet, consectetur adipiscing elit.33333",
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Be yourself, everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
 ];
 
 const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
 const generateButton = document.getElementById("generate-btn");
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
+  // const quote = randomQuote.quote;
+  // const author = randomQuote.author;
+  //const { quote, author } = randomQuote;
 
-  quoteElement.textContent = randomQuote;
+  quoteElement.textContent = randomQuote.quote;
+  authorElement.textContent = randomQuote.author;
 }
 
 generateButton.addEventListener("click", generateRandomQuote);
