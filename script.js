a0a8122019a82c06a0bfe4c29d420f5dd7bd7710
kniazivna <olenkaknyaz@gmail.com>
2026-07-11 18:43:25 +0300

    add js module, make file quotes.js

--- script.js
+++ script.js
@@ -1,49 +1,6 @@
 "use strict";
 
-const quotes = [
-  {
-    quote: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Be yourself, everyone else is already taken.",
-    author: "Oscar Wilde",
-  },
-  {
-    quote: "In the middle of every difficulty lies opportunity.",
-    author: "Albert Einstein",
-  },
-  {
-    quote:
-      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
-    author: "Winston Churchill",
-  },
-  {
-    quote: "It always seems impossible until it's done.",
-    author: "Nelson Mandela",
-  },
-  {
-    quote: "Life is what happens when you're busy making other plans.",
-    author: "John Lennon",
-  },
-  {
-    quote:
-      "The future belongs to those who believe in the beauty of their dreams.",
-    author: "Eleanor Roosevelt",
-  },
-  {
-    quote: "Do what you can, with what you have, where you are.",
-    author: "Theodore Roosevelt",
-  },
-  {
-    quote: "Happiness depends upon ourselves.",
-    author: "Aristotle",
-  },
-  {
-    quote: "The best way to predict the future is to create it.",
-    author: "Peter Drucker",
-  },
-];
+import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
