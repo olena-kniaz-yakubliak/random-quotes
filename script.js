4ec30181b4e97c9a993d780e802730920239cdd4
kniazivna <olenkaknyaz@gmail.com>
2026-07-14 14:48:06 +0300

    refactored the code and broke down large functions into smaller ones

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const faviritesContainer = document.getElementById("favorites-container");
 
 let currentQuoteIndex;
 
+function toggleFavoritesIcon(isFavorite) {
+  favoriteToggleButton.classList.toggle("fa", isFavorite);
+  favoriteToggleButton.classList.toggle("far", !isFavorite);
+}
+
 function generateRandomQuote() {
   currentQuoteIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[currentQuoteIndex];
@@ -20,47 +25,44 @@ function generateRandomQuote() {
 
   quoteElement.textContent = randomQuote.quote;
   authorElement.textContent = randomQuote.author;
-  if (randomQuote.isFavorite) {
-    favoriteToggleButton.classList.add("fa");
-    favoriteToggleButton.classList.remove("far");
-  } else {
-    favoriteToggleButton.classList.remove("fa");
-    favoriteToggleButton.classList.add("far");
-  }
+  toggleFavoritesIcon(randomQuote.isFavorite);
   favoriteToggleButton.style.display = "inline-block";
+}
+
+function showFavoriteCard(quote, author) {
+  const favoriteCard = document.createElement("div");
+  favoriteCard.classList.add("favorite-card");
+  favoriteCard.innerHTML = `
+      <p>${quote}</p>
+      <p class = 'author'>${author}</p>
+      `;
+  faviritesContainer.appendChild(favoriteCard);
+}
+
+function hideFavoriteCard(quote) {
+  const favoriteCards = document.querySelectorAll(".favorite-card");
+  favoriteCards.forEach((card) => {
+    if (card.textContent.includes(quote)) {
+      card.remove();
+    }
+  });
 }
 
 function toggleFavorites() {
   const currentQuote = quotes[currentQuoteIndex];
   currentQuote.isFavorite = !currentQuote.isFavorite;
-  if (currentQuote.isFavorite) {
-    favoriteToggleButton.classList.add("fa");
-    favoriteToggleButton.classList.remove("far");
-  } else {
-    favoriteToggleButton.classList.remove("fa");
-    favoriteToggleButton.classList.add("far");
-  }
+  toggleFavoritesIcon(currentQuote.isFavorite);
+  favoriteToggleButton.style.display = "inline-block";
 
   if (currentQuote.isFavorite) {
-    const favoriteCard = document.createElement("div");
-    favoriteCard.classList.add("favorite-card");
-    favoriteCard.innerHTML = `
-      <p>${currentQuote.quote}</p>
-      <p class = 'author'>${currentQuote.author}</p>
-      `;
-    faviritesContainer.appendChild(favoriteCard);
+    showFavoriteCard(currentQuote.quote, currentQuote.author);
   } else {
     //remove card when is unfavorite
-    const favoriteCards = document.querySelectorAll(".favorite-card");
-    favoriteCards.forEach((card) => {
-      if (card.textContent.includes(currentQuote.quote)) {
-        card.remove();
-      }
-    });
+    hideFavoriteCard(currentQuote.quote);
   }
 }
 
 generateButton.addEventListener("click", generateRandomQuote);
 favoriteToggleButton.addEventListener("click", toggleFavorites);
 
-//generateRandomQuote();
+generateRandomQuote();
